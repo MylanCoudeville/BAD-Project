40da65821bdb964071d81071ffb6d30f02059716
MylanCoudeville <mylan.coudeville@student.odisee.be>
2023-05-24 17:19:07 +0200

    Contact UI

---
+++ B-Rock/B-Rock/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B_Rock.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
