bc8f32d6f4e7f4a54f9e9511b4674dcb6d5e3949
MylanCoudeville <mylan.coudeville@student.odisee.be>
2023-06-08 16:01:33 +0200

    Before Autotication

---
+++ B-Rock/B-Rock/Controllers/ReservationController.cs
@@ -0,0 +1,26 @@
+﻿using B_Rock.Data;
+using B_Rock.Models.Calendar;
+using B_Rock.Models.Reservation;
+using B_Rock.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B_Rock.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly ConcertService _concertService;
+        public ReservationController(ConcertService concertService)
+        {
+            _concertService = concertService;
+        }
+        public IActionResult Index(int concertId, int Quantity)
+        {
+            Concert c = _concertService.GetById(concertId);
+            CheckoutReservationViewModel viewModel= new CheckoutReservationViewModel() {
+                ConcertId = c.Id,
+                Title = c.Title
+            };
+            return View();
+        }
+    }
+}
---
+++ B-Rock/B-Rock/Models/Reservation/CheckoutReservationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace B_Rock.Models.Reservation
+{
+    public class CheckoutReservationViewModel
+    {
+        public int ConcertId { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        [Display(Name = "Date and time")]
+        public DateTime DateAndTime { get; set; }
+        [Display(Name = "Total price")]
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        [Display(Name = "ZIP-code")]
+        public string ZIPCode { get; set; }
+    }
+}
